e5463ea7aca80990fb06e38a0b42ee859df106b9
Fabio Cesso <fabio.cesso@gmail.com>
2018-06-02 12:15:57 -0300

    feat(header): lançamento da versão beta
    Alteração do componente de scroll

--- AmadeusSite_v0/js/amadeus.js
+++ AmadeusSite_v0/js/amadeus.js
@@ -19,7 +19,19 @@ $(function () {
         cursorwidth: "8px",
         cursorborderradius: "10px",
         cursorborder: "none",
+		grabcursorenabled: false,
+		autohidemode: true
     });
+	$('.nicescroll-rails').click(function(e) {
+		var mouseY = e.pageY - $('html').scrollTop();
+		var scrollY = this.childNodes[0].offsetTop;
+		if (mouseY > scrollY && mouseY < (scrollY + 112)) {
+			return;
+		}
+		$('html,body').animate({
+                    scrollTop: mouseY < scrollY ? $('html').scrollTop() - window.outerHeight + 30 : $('html').scrollTop() + window.outerHeight - 30
+                }, 1000);
+	});
 
     $('a[href*=#]:not([href=#])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
@@ -34,10 +46,10 @@ $(function () {
         }
     });
 
-    $('#section-main').css({ 'height': (($(window).height())) + 'px' });
+    /*$('header').css({ 'min-height': (($(window).height())) + 'px' });
     $(window).resize(function () {
-        $('#section-main').css({ 'height': (($(window).height())) + 'px' });
-    });
+        $('header').css({ 'min-height': (($(window).height())) + 'px' });
+    });*/
 
     $(document).on('click', '#section-news .news-card', function () {
         window.open($(this).attr('data-news-url'));
